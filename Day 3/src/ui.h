ae753e852cbd1f5ba5baa6abcf2f83d1b492942e
LloydTTG <randyngui08@gmail.com>
2026-04-02 21:55:42 +0800

    Add ui.h header file with display functions

---
+++ Day 3/src/ui.h
@@ -0,0 +1,10 @@
+#include <Adafruit_GFX.h>
+#include <U8g2lib.h>
+
+extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
+
+void neutral(void);
+void neutral_2(void);
+void neutral_3(void);
+void evil(void);
+void bright(void);
